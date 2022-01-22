d672b55cd029f652bd912c7d027d2c00afa58d56
lutfi <lutfirid@gmail.com>
2022-01-22 21:57:14 +0700

    dev-contact

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "USEREMAIL@gmail.com";
+        $mail_to = "lutfirid@gmail.com";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
@@ -15,7 +15,7 @@
         if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Lengkapi Data Untuk Dikirim.";
             exit;
         }
         
@@ -34,17 +34,17 @@
         if ($success) {
             # Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Terima kasih! Pesan Anda telah dikirim.";
         } else {
             # Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong, we couldn't send your message.";
+            echo "Ups! Ada yang tidak beres, kami tidak dapat mengirim pesan Anda.";
         }
 
     } else {
         # Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Ada masalah saat pengiriman, silakan coba lagi.";
     }
 
 ?>
